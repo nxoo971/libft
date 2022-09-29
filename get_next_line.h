9f4c4cdddae81b5946b03e0264d1696a912470a8
jeanmarc <jmwanc@gmail.com>
2022-09-29 21:38:28 +0200

    Fix norm

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,12 @@
 /*   By: nxoo <nxoo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 23:23:09 by ooxn              #+#    #+#             */
-/*   Updated: 2022/09/29 21:30:25 by nxoo             ###   ########.fr       */
+/*   Updated: 2022/09/29 21:38:12 by nxoo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <fcntl.h>
 # include "libft.h"
